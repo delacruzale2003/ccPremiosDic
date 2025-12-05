53aec9a450694660caced5f15b429f9910ba7e4e
unknown <alejandro>
2025-12-05 08:17:49 -0500

    registerPage updated

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -105,7 +105,7 @@ const RegisterPage: React.FC = () => {
                         />
                     </div>
                 </div>
-
+                    
                 {/* Campo Teléfono */}
                 <div className="">
                     <label className="block text-white text-md font-medium font-mont-extrabold mt-2">Número de teléfono</label>
